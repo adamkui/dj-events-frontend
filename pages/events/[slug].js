7c42f59313887377a43a540e770c2eb05743d87d
Kui Ádám <74501875+adamkui@users.noreply.github.com>
2021-05-24 20:05:47 +0200

    update for build

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -45,6 +45,7 @@ export default function EventPage({evt}) {
   )
 }
 
+/*
 export async function getStaticPaths(){
   const res = await fetch(`${API_URL}/events`)
   const events = await res.json()
@@ -69,11 +70,10 @@ export async function getStaticProps({params:{slug}}){
     revalidate: 1
   }
 }
+*/
 
-
-/*
 export async function getServerSideProps({query:{slug}}){
-  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
   const events = await res.json();
   return {
     props: {
@@ -81,4 +81,3 @@ export async function getServerSideProps({query:{slug}}){
     }
   }
 }
-*/
